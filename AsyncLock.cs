673d13a4dd99daec817b377fd230549b0fa7a9b7
Simon G <simon.gockner@gmail.com>
2024-03-22 14:57:26 +0100

    - add async lock

---
+++ AsyncLock.cs
@@ -0,0 +1,21 @@
+﻿// Author: Simon.Gockner
+// Created: 2024-03-22
+// Copyright(c) 2024 SimonG. All Rights Reserved.
+
+namespace Lib.Tools;
+
+public class AsyncLock
+{
+    private readonly SemaphoreSlim _semaphoreSlim = new(1, 1);
+
+    public async Task<Lock> Lock()
+    {
+        await _semaphoreSlim.WaitAsync();
+        return new Lock(_semaphoreSlim);
+    }
+}
+
+public class Lock(SemaphoreSlim semaphoreSlim) : IDisposable
+{
+    public void Dispose() => semaphoreSlim.Release();
+}
