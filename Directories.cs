a0f1faf7d8769079aa26596bf804ed207bb6b6da
Simon G. <simon.gockner@gmail.com>
2024-09-17 20:18:58 +0200

    - add directories extension methods, add clean method

---
+++ Directories.cs
@@ -0,0 +1,17 @@
+﻿// Author: simon
+// Created: 2024-09-17
+// Copyright(c) 2024 SimonG. All Rights Reserved.
+
+namespace Lib.Tools;
+
+public static class Directories
+{
+    public static void Clean(this DirectoryInfo directory)
+    {
+        foreach (FileInfo file in directory.EnumerateFiles()) 
+            file.Delete();
+
+        foreach (DirectoryInfo subDirectory in directory.EnumerateDirectories()) 
+            subDirectory.Delete(true);
+    }
+}
