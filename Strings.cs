ac5eda246665ef65a4f63a5f899593d176bc055f
Simon G. <simon.gockner@gmail.com>
2024-09-20 17:23:36 +0200

    - add removeWhitespace method

---
+++ Strings.cs
@@ -0,0 +1,15 @@
+﻿// Author: simon
+// Created: 2024-09-20
+// Copyright(c) 2024 SimonG. All Rights Reserved.
+
+using System.Text.RegularExpressions;
+
+namespace Lib.Tools;
+
+public static partial class Strings
+{
+    [GeneratedRegex(@"\s+", RegexOptions.Compiled)]
+    private static partial Regex WhitespaceRegex();
+    
+    public static string RemoveWhitespaces(this string input) => WhitespaceRegex().Replace(input, "");
+}
