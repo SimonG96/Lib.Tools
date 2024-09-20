7266661625b12d477b7250ea8671ecf3af5a5bc0
Simon G. <simon.gockner@gmail.com>
2024-09-20 16:58:32 +0200

    - add json fileHandler

---
+++ Json/JsonFileHandler.cs
@@ -0,0 +1,35 @@
+﻿// Author: simon
+// Created: 2024-09-20
+// Copyright(c) 2024 SimonG. All Rights Reserved.
+
+using System.Text.Json;
+
+namespace Lib.Tools.Json;
+
+public class JsonFileHandler<TInterface, TImplementation> where TImplementation : TInterface where TInterface : notnull
+{
+    private readonly JsonSerializerOptions _jsonSerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true,
+        WriteIndented = true,
+        Converters = { new AbstractConverter<TInterface, TImplementation>() }
+    };
+
+    public async Task WriteFile(TInterface value, string filePath)
+    {
+        await using FileStream fileStream = File.OpenWrite(filePath);
+        await JsonSerializer.SerializeAsync(fileStream, value, _jsonSerializerOptions);
+    }
+
+    public async Task WriteToExistingFile(TInterface value, string filePath)
+    {
+        await using FileStream fileStream = File.Open(filePath, FileMode.Truncate, FileAccess.Write);
+        await JsonSerializer.SerializeAsync(fileStream, value, _jsonSerializerOptions);
+    }
+
+    public async Task<TInterface> ReadFile(string filePath)
+    {
+        await using FileStream fileStream = File.OpenRead(filePath);
+        return await JsonSerializer.DeserializeAsync<TInterface>(fileStream, _jsonSerializerOptions) ?? throw new Exception($"Invalid {typeof(TInterface)} file.");
+    }
+}
