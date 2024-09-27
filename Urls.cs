cefaf59a6b78c3c38599e62e95f637edf8830b97
Simon G. <simon.gockner@gmail.com>
2024-09-27 18:12:33 +0200

    - add method to open urls in browser platform independently

---
+++ Urls.cs
@@ -0,0 +1,28 @@
+﻿// Author: simon
+// Created: 2024-09-27
+// Copyright(c) 2024 SimonG. All Rights Reserved.
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Lib.Tools;
+
+public static class Urls
+{
+    public static void OpenUrlInBrowser(string url)
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            url = url.Replace("&", "^&");
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", url);
+        }
+    }
+}
