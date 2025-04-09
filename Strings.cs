285cd8d65a38f2c3b82e0a81dc87122dc3192f96
Simon G. <simon.gockner@gmail.com>
2025-04-09 10:39:01 +0200

    - add readLines method and async overload

--- Strings.cs
+++ Strings.cs
@@ -2,6 +2,7 @@
 // Created: 2024-09-20
 // Copyright(c) 2024 SimonG. All Rights Reserved.
 
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
 namespace Lib.Tools;
@@ -12,4 +13,18 @@ public static partial class Strings
     private static partial Regex WhitespaceRegex();
     
     public static string RemoveWhitespaces(this string input) => WhitespaceRegex().Replace(input, "");
+
+    public static IEnumerable<string> ReadLines(this string input)
+    {
+        using StringReader stringReader = new(input);
+        while (stringReader.ReadLine() is { } line)
+            yield return line;
+    }
+    
+    public static async IAsyncEnumerable<string> ReadLinesAsync(this string input, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        using StringReader stringReader = new(input);
+        while (await stringReader.ReadLineAsync(cancellationToken).ConfigureAwait(false) is { } line)
+            yield return line;
+    }
 }
